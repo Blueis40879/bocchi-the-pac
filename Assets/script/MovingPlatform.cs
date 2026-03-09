45e9db9ff4483ea69ab2c9aad863fd07986aab46
shikanokonokonokokoshitantan <jakubszymanzki@gmail.com>
2026-03-09 12:43:54 +0100

    porno
    porno

--- Assets/script/MovingPlatform.cs
+++ Assets/script/MovingPlatform.cs
@@ -1,5 +1,6 @@
 using System.Numerics;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class MovingPlatform : MonoBehaviour
 {
@@ -27,13 +28,6 @@ public class MovingPlatform : MonoBehaviour
         transform.position = UnityEngine.Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            collision.gameObject.transform.SetParent(transform);
-        }
-    }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
