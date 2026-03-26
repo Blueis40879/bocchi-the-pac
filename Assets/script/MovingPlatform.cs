1d848e74634337f36a9f17a5f8af2d20df9a09fc
shikanokonokonokokoshitantan <jakubszymanzki@gmail.com>
2026-03-26 13:49:04 +0100

    pathfinding
    pathfinding

--- Assets/script/MovingPlatform.cs
+++ Assets/script/MovingPlatform.cs
@@ -1,31 +1,41 @@
 using System.Numerics;
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.AI;
 
 public class MovingPlatform : MonoBehaviour
 {
     public float speed = 2f;
     public Transform [] points;
+    [SerializeField] Transform target;
+    NavMeshAgent agent;
     private int i;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //transform.position = points[0].position;
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+    void Awake()
+    {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (UnityEngine.Vector2.Distance(transform.position, points[i].position) < 0.1f)
-        {
-            i++;
-            if (i == points.Length)
-            {
-                i = 0;
-            }
-        }
+        //if (UnityEngine.Vector2.Distance(transform.position, points[i].position) < 0.1f)
+        //{
+        //    i++;
+        //    if (i == points.Length)
+        //    {
+       //         i = 0;
+       //     }
+       // }
 
-        transform.position = UnityEngine.Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+        //transform.position = UnityEngine.Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+        agent.SetDestination(target.position);
     }
 
 }
